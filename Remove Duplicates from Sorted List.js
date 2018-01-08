9e6590e07b2fa7bc97ee653ff8c99f7a68fc2ac4
Rouzip <1226015390@qq.com>
2018-01-08 11:17:46 +0800

    改正链表去重

--- Remove Duplicates from Sorted List.js
+++ Remove Duplicates from Sorted List.js
@@ -10,16 +10,10 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-	let result = head;
-	if (!head || !head.next){
-		return head;
+	for (let node = head;node;node = node.next){
+		while(node.next && node.val == node.next.val)
+			node.next = node.next.next;
 	}
-	while(head.next){
-		if (head.val == head.next.val){
-			head = head.next;
-			continue;
-		}
-		result.next = head;
-	}
-	return result;
+  return head;
 };
+//链表去重，对于链表的操作还需要熟练，一开始纠结于各种情况，属于钻牛脚尖了
