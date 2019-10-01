9a9c5eddb2bc05b47ebcd6535626b3aaa0f81eca
Rouzip <1226015390@qq.com>
2019-10-01 21:46:33 +0800

    add new method📖

--- Flipping an Image.go
+++ Flipping an Image.go
@@ -11,3 +11,18 @@ func flipAndInvertImage(A [][]int) [][]int {
 	}
 	return res
 }
+
+func flipAndInvertImage(A [][]int) [][]int {
+	length := len(A)
+	res := make([][]int, length)
+	for i := 0; i < length; i++ {
+		res[i] = make([]int, length)
+	}
+	for i := 0; i < length; i++ {
+		for j := 0; j < (length+1)/2; j++ {
+			res[i][j] = A[i][length-1-j] ^ 1
+			res[i][length-1-j] = A[i][j] ^ 1
+		}
+	}
+	return res
+}
